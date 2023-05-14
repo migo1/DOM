808eab2916d7119ac4a6a73183e294d11dcd3c19
migo1 <ertmigot@gmail.com>
2023-05-14 19:35:49 +0300

    project 2

---
+++ project2/app.js
@@ -0,0 +1,28 @@
+// Create variable
+
+let openBtn = document.getElementById("open-btn");
+let modalContainer = document.getElementById("modal-container");
+let closeBtn = document.getElementById("close-btn");
+
+// Event Listeners
+
+openBtn.addEventListener("click", toggleModal);
+
+closeBtn.addEventListener("click", toggleModal);
+
+
+function toggleModal() {
+  if (modalContainer.style.display === "block") {
+    modalContainer.style.display = "none";
+  } else {
+    modalContainer.style.display = "block";
+  }
+}
+
+window.addEventListener('click', function (e) {
+    
+    if (e.target === modalContainer) {
+        modalContainer.style.display = "none";
+    }
+
+})
